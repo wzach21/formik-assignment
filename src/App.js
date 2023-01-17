309cc3c2fff79ac37fe232e1fe4bea04aa0263a5
wzach21 <zachlw21@gmail.com>
2023-01-16 23:29:53 -0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useFormik } from 'formik';
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name:'',
+      email:'',
+      password:''
+       },
+      onSubmit: values =>{
+        console.log("form:", values);
+      },
+  validate: values => {
+    let errors = {};
+    if(!values.email) {
+      errors.email = 'Field required.'; 
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+          values.email
+        )
+    ) {
+          errors.email = 'Username should be email';
+        }
+    if(!values.password) errors.password = 'Field required.';
+    if(!errors.email && !errors.password){window.alert('Loging Succesful!')};
+    return errors;
+  }
+  
+
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email</div>
+        <input type="text" name="email" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>: null} 
+        <div>Password</div>
+        <input type="text" name="password" onChange={formik.handleChange} value={formik.values.password}/>
+        {formik.errors.password ? <div style={{color:'red'}}>{formik.errors.password}</div>: null} 
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
